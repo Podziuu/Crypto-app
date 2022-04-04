45d7084f629abb7bb46833c6fd789774207ac387
Bartosz Podemski <bartekpodemski@gmail.com>
2022-04-04 16:22:51 +0200

    Fixing responsive problem

--- my-app/src/components/Crypto.js
+++ my-app/src/components/Crypto.js
@@ -36,7 +36,7 @@ const Crypto = () => {
           Show More
         </Link>
       </div>
-      <div className="grid grid-rows-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-6 gap-x-5 px-4">
+      <div className="grid grid-rows-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-6 gap-x-5 pr-4">
         {cryptos.map((crypto) => {
           return <CryptoDetail key={crypto.uuid} crypto={crypto} />;
         })}
